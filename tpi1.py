0a5d61ca4d86445ac667769da54cfdc5e10fa970
dianarrmiranda <dianarrmiranda@ua.pt>
2023-10-28 20:40:46 +0100

    ex1, ex2 and ex3 done

--- tpi1.py
+++ tpi1.py
@@ -1,11 +1,11 @@
-#STUDENT NAME:
-#STUDENT NUMBER:
+#STUDENT NAME: Diana Miranda
+#STUDENT NUMBER: 107457
 
 #DISCUSSED TPI-1 WITH: (names and numbers):
 
 
 from tree_search import *
-
+import math
 class OrderDelivery(SearchDomain):
 
     def __init__(self,connections, coordinates):
@@ -24,19 +24,15 @@ class OrderDelivery(SearchDomain):
         return actlist 
 
     def result(self,state,action):
-        #IMPLEMENT HERE
         pass
 
     def satisfies(self, state, goal):
-        #IMPLEMENT HERE
         pass
-
+    
     def cost(self, state, action):
-        #IMPLEMENT HERE
         pass
 
     def heuristic(self, state, goal):
-        #IMPLEMENT HERE
         pass
 
 
@@ -46,25 +42,85 @@ class MyNode(SearchNode):
     def __init__(self,state,parent,arg3=None,arg4=None,arg5=None,arg6=None):
         super().__init__(state,parent)
         #ADD HERE ANY CODE YOU NEED
+        self.depth = arg3
+        self.cost = arg4
+        self.heuristic = arg5
+        self.eval = arg6
+        self.children = []
+        self.markedForDel = False
+    
+    def in_parent(self, newstate):
+        if self.parent == None:
+            return False
+        if self.parent.state == newstate:
+            return True
+        return self.parent.in_parent(newstate)
+
 
 class MyTree(SearchTree):
 
     def __init__(self,problem, strategy='breadth',maxsize=None):
         super().__init__(problem,strategy)
         #ADD HERE ANY CODE YOU NEED
+        root = MyNode(problem.initial, None, 0, 0, 0)
+        self.open_nodes = [root]
+        self.terminals = 0
+        self.maxsize = maxsize
 
     def astar_add_to_open(self,lnewnodes):
-        #IMPLEMENT HERE
-        pass
+        for node in lnewnodes:
+            self.open_nodes.append(node)
+            self.open_nodes.sort(key= lambda node: (node.cost + node.heuristic, node.state))
+
 
     def search2(self):
-        #IMPLEMENT HERE
-        pass
+        while self.open_nodes != []:
+            self.terminals = len(self.open_nodes)
+            node = self.open_nodes.pop(0)
+            if self.problem.goal_test(node.state):
+                self.solution = node
+                return self.get_path(node)
+            
+            lnewnodes = []
+            self.non_terminals +=1
+            for a in self.problem.domain.actions(node.state):
+                newstate = self.problem.domain.result(node.state,a)
+                if not node.in_parent(newstate):
+                    newnode = MyNode(newstate,node, node.depth + 1, node.cost + self.problem.domain.cost(node.state, a), self.problem.domain.heuristic(newstate, self.problem.goal))
+                    lnewnodes.append(newnode)
+
+                    newnode.eval = newnode.cost + newnode.heuristic
+                    node.children.append(newnode)
+
+            self.add_to_open(lnewnodes)
+            self.manage_memory()
+
+        return None
 
     def manage_memory(self):
-        #IMPLEMENT HERE
-        pass
+        if self.strategy != 'A*' or self.maxsize is None:
+            return  
+            
+        while len(self.open_nodes) > self.maxsize:
+            nodeToDel = self.open_nodes.pop(-1)
+            nodeToDel.markedForDel = True
 
+            allToDel = True
+            for brother in nodeToDel.parent.children:
+                if brother.markedForDel == False:
+                    allToDel = False
+                    break
+
+            if allToDel:
+                for brother in nodeToDel.parent.children:
+                    if brother in self.open_nodes: 
+                        self.open_nodes.remove(brother)
+                    
+
+            
+
+
+ 
     # if needed, auxiliary methods can be added here
 
 
